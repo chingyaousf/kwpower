db17b8d1c8c9a57bfddea7b121628a5173a32a7c
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-04-27 17:18:43 -0400

    Add files via upload

---
+++ R/tampa_temp_data_2017_2022_documentation.R
@@ -0,0 +1,15 @@
+#' Tampa Monthly Temperature Data 2017-2022
+#'
+#' This data set contains monthly temperature data for Tampa from 2017 to 2022
+#'
+#' @name tampa_temp_data_2017_2022
+#' @docType data
+#' @keywords dataset
+#' @usage data(tampa_temp_data_2017_2022)
+#'
+#' @return A data frame object containing the Tampa monthly temperature data 2017-2022
+#'
+#' @examples
+#' data(tampa_temp_data_2017_2022)
+tampa_temp_data <- read.csv("./data/2017-2022 Tampa monthly temperature.csv")
+
