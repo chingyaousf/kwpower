bb9be96dd3fe5a00696f49bd58bcbf454cc29d56
chingyaousf <96981462+chingyaousf@users.noreply.github.com>
2023-04-27 17:15:54 -0400

    Add files via upload

---
+++ man/tampa_temp_data_documentation.R
@@ -0,0 +1,15 @@
+#' Tampa Monthly Temperature Data
+#'
+#' This data set contains monthly temperature data for Tampa from 1900 to 2022.
+#'
+#' @name tampa_temp_data
+#' @docType data
+#' @keywords dataset
+#' @usage data(tampa_temp_data)
+#'
+#' @return A data frame object containing the Tampa monthly temperature data.
+#'
+#' @examples
+#' data(tampa_temp_data)
+tampa_temp_data <- read.csv("./data/1900 1990 2022 Tampa monthly temperature.csv")
+
